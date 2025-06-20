baee280d828404901c459c20491a044acb39d818
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-06-20 11:23:49 +0200

    function that prints the alphabet, in lowercase, followed by a new line

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n is greater than zero, -1 if n is less than zero,
+ * and 0 if n is zero.
+ */
+int print_sign(int n)
+{
+  if (n > 0)
+  {
+    _putchar('+');
+    return (1);
+  }
+  else if (n < 0)
+  {
+    _putchar('-');
+    return (-1);
+  }
+  else
+  {
+    _putchar('0');
+    return (0);
+  }
+}
