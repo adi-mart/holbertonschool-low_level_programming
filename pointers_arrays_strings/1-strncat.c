0068768568de1fb118f1c4dab01afb7cd13892fb
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-06-26 23:19:35 +0200

    function that concatenates two strings with 'n' which counts the number of characters we want to display

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,20 +10,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    j = 0;
-    i = 0;
+	j = 0;
+	i = 0;
 
-    while (dest[i] != '\0')
-        i++;
-    while (j < n && src[j] != '\0')
-    {
-        dest[i + j] = src[j];
-        j++;
-    }
-    dest[i + j] = '\0';
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
 
-    return dest;
+	return (dest);
 }
