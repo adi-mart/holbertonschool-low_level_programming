115e3c3740bd332547d7bde4dbd44434a17eafae
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-06-27 09:35:11 +0200

    function that copy two strings with 'n' which counts the number of characters we want to display

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - copies up to n characters from the string pointed to by src
+ * to the buffer pointed to by dest.
+ * @dest: pointer to the destination buffer
+ * @src: pointer to the source string
+ * @n: number of characters to copy from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
