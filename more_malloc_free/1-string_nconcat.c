b3563d1dfbeaa87beb96741fd9cee88413d2524f
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-08 16:23:48 +0200

    function that concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,12 +19,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
+
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
+
 	while (s2[len2] != '\0')
 		len2++;
 
