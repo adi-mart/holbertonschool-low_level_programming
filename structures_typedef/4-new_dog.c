571242ddf0de52864495bd7561c75c8b9155c03d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-09 22:19:42 +0200

    Refactor new_dog function to ensure proper memory allocation and copying of name and owner fields

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -12,25 +13,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	char *name_cpy;
+	char *owner_cpy;
 
+	if (name == NULL)
+		return (NULL);
+	if (owner == NULL)
+		return (NULL);
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	if (name == NULL)
+	name_cpy = strdup(name);
+	if (name_cpy == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (owner == NULL)
+	owner_cpy = strdup(owner);
+	if (owner_cpy == NULL)
 	{
-		free(name);
+		free(name_cpy);
 		free(ptr);
 		return (NULL);
 	}
-	ptr->name = name;
-	ptr->owner = owner;
+	ptr->name = name_cpy;
+	ptr->owner = owner_cpy;
 	ptr->age = age;
 
 	return (ptr);
