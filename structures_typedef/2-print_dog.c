f2d6360c7e11434f6eb7ff2300889823bcd8ec2c
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-09 11:15:51 +0200

    Add print_dog function to print details of a struct dog and update header file

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <math.h>
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to the struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf(" ");
+	}
+	if (d->name == NULL || d->owner == NULL || d->age == NAN)
+		printf("nil\n");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
