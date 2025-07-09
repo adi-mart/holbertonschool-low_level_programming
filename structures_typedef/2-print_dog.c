af7dd66dae8510a83dc68fe9bbefd1e617f81ac2
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-09 14:15:50 +0200

    Fix print_dog function to check only for NULL name before printing

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->name == NULL || d->owner == NULL || d->age == NAN)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
