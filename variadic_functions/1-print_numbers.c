997038e96dcfe74369cfb552b65eff417b16082b
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-16 18:09:17 +0200

    Fix: Remove unnecessary NULL check for separator in print_numbers function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,13 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	i = 0;
 
-	if (separator == NULL)
-		return;
 	va_start(args, n);
 	while (i < n)
 	{
 		printf("%d", va_arg(args, int));
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 		i++;
 	}
