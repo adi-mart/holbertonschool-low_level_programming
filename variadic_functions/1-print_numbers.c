c75c50dafd7ce414bb2c46f838ae5e4831d9ca5d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-16 17:48:38 +0200

    Add print_numbers function to print integers with a separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers, followed by a separator
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	i = 0;
+
+	va_start(args, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1)
+			printf("%s", separator);
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
