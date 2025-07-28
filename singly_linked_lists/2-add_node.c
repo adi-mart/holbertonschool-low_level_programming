0fc17841328ef40da7f9991583a1f7a0dabde5e1
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-28 17:11:28 +0200

    improve null check in add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,13 +12,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
-	if (head == NULL)
+	if (head == NULL || new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	if (new == NULL)
-		return (NULL);
 
 	new->str = strdup(str);
 	new->len = strlen(str);
