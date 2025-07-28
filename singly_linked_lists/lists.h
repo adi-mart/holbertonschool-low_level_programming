cb56cb96cfcb3c756405e9ef512abff664718de7
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-28 15:13:29 +0200

    function that prints all the elements of a list_t list

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+/**
+ * struct listint_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	char *str;
+	unsigned int len;
+	struct listint_s *next;
+
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
