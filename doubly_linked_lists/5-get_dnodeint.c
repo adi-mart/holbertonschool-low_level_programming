61ee0f665e8e181dc716232a4b74646f8a984dde
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-07-30 09:49:47 +0200

    function that returns the sum of all the data (n) of a dlistint_t linked list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,16 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
-
+/**
+ * get_dnodeint_at_index - retrieves the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node to retrieve
+ *
+ * Return: pointer to the node at the specified index, or NULL if not found
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (head != NULL)
-    {
-        if (i == index)
-            return head;
-        head = head->next;
-        i++;
-    }
-    return NULL;
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
 }
---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - calculates the sum of all the data in a dlistint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: sum of all the data in the list, or 0 if empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
