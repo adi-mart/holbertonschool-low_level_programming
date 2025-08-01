292e2ad3b26949b1a643e5bf6369a096e2e25022
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-01 11:00:34 +0200

    fix formatting and indentation in append_text_to_file function

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/** * append_text_to_file - appends text content to a file
+/**
+ * append_text_to_file - appends text content to a file
  * @filename: name of the file to append to
  * @text_content: NULL terminated string to append to the file
  *
  * Return: 1 on success, -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
@@ -17,20 +17,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-    chmod(filename, 0664);
+	chmod(filename, 0664);
 
 	if (text_content)
 	{
 		while (text_content[len])
 			len++;
 
-        if (write(fd, text_content, len) == -1)
-        {
-            close(fd);
-            return (-1);
-        }
+		if (write(fd, text_content, len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 
 	}
 	close(fd);
-	    return (1);
+		return (1);
 }
