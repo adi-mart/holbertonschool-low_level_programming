edd470b3d2578a5ed575a104636374182c82a6d3
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-02 16:09:51 +0200

    refactor: move size validation to the beginning of hash_table_create function

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,12 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new;
 	unsigned long int i = 0;
 
+	if (size == 0)
+		return (NULL);
+
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
 
-	if (size == 0)
-		return (NULL);
 	new->size = size;
 	new->array = malloc(size * sizeof(*new->array));
 	if (new->array == NULL)
