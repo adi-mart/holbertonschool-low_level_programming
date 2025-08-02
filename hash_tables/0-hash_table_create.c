d9244c558475a7caf837487331e00930c8c9692a
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-02 15:53:06 +0200

    implement hash table data structures and creation function

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array to be created
+ *
+ * Return: pointer to the newly created hash table,
+ * or NULL if it failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long int i = 0;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->size = size;
+	new->array = malloc(size * sizeof(*new->array));
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
+	return (new);
+}
