a9f59c4f8cd76eab950cbab2092affd332355a44
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-03 22:50:21 +0200

    fix: correct file opening mode for destination file in 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	fd_to = open(argv[2], 577, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((r = read(fd_from, buf, 1024)) > 0)
