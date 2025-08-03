c1ecf69028accc61e4ff3873d7b5cb282c643bd0
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-03 17:07:13 +0200

    refactor hash_table_print function to improve readability and maintainability

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -11,22 +11,34 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
 	unsigned long int i = 0;
+	size_t count = 0;
+	size_t total = 0;
 
 	if (ht == NULL)
 		return;
+
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+			total++;
+		i++;
+	}
+
+	i = 0;
 
 	printf("{");
 	while (i < ht->size)
 	{
 		current = ht->array[i];
-		while (current != NULL)
+		while (current)
 		{
 			printf("'%s': '%s'", current->key, current->value);
-			current = current->next;
-			if (current != NULL)
+			count++;
+			if (count < total - 1)
 				printf(", ");
+			current = current->next;
 		}
 		i++;
 	}
 	printf("}\n");
-}
+	}
