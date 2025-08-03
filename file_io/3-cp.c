c4b45a1e7ceb72cc9509dc0fe3750cfcd24f2a36
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-03 23:04:32 +0200

    fix: improve error handling for file write operation in 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        close(file_from);
-        exit(99);
-    }
-    fchmod(fd_to, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+	fchmod(fd_to, 0664);
 	while ((r = read(file_from, buf, BUF_SIZE)) > 0)
 	{
 		w = write(fd_to, buf, r);
